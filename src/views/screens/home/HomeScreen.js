84ec9d28c7b004895097aac2e0907fcb0edf51b5
mzfais <mzfais@gmail.com>
2023-10-18 05:48:38 +0700

    add styles to homescreen

--- src/views/screens/home/HomeScreen.js
+++ src/views/screens/home/HomeScreen.js
@@ -1,24 +1,45 @@
-import {View} from 'react-native';
+import {View, TextInput, StyleSheet} from 'react-native';
 import React from 'react';
-import {Button, TextInput} from 'react-native-paper';
+import Icon from 'react-native-vector-icons/Ionicons';
 
+const styles = StyleSheet.create({
+  container: {
+    margin: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  searchBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 32,
+    borderColor: 'gray',
+    padding: 10,
+    gap: 10,
+    marginHorizontal: 5,
+    flex: 1,
+  },
+  borderShadow: {
+    borderRadius: 50,
+    shadowColor: 'gray',
+    shadowOffset: {
+      width: 2,
+      height: 1,
+    },
+    shadowOpacity: 5,
+    shadowRadius: 2,
+    elevation: 5,
+    borderTopWidth: 0,
+    borderLeftWidth: 0.2,
+    backgroundColor: '#f5f5f5',
+  },
+});
 export default function HomeScreen({navigation}) {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        gap: 20,
-        margin: 20,
-      }}>
-      <TextInput label="Email" />
-      {/* mode="text" menampilkan button dalam mode text, contained seperti button */}
-      <Button
-        mode="contained"
-        icon="arrow-right-circle"
-        onPress={() => navigation.navigate('Detail', {itemId: 40})}>
-        Menuju ke Detail
-      </Button>
+    <View>
+      <View>
+        <Icon name="search" size={12} />
+        <TextInput placeholder="Mau cari barang apa?" />
+      </View>
     </View>
   );
 }
