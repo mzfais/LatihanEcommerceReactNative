b13b960e31a47b65a40e3d268c143ea96f340961
mzfais <mzfais@gmail.com>
2023-10-19 12:51:17 +0700

    setting plugin di babel utk menghindari error setelah install package gesture handle dan reanimated

--- babel.config.js
+++ babel.config.js
@@ -5,4 +5,14 @@ module.exports = {
       plugins: ['react-native-paper/babel'],
     },
   },
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.tsx', '.ts', '.js', '.json'],
+      },
+    ],
+    'react-native-paper/babel',
+    'react-native-reanimated/plugin',
+  ],
 };
