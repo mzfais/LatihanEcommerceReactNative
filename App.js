76698088e5d5984c8a58a9baada7637b0a2aa0b5
mzfais <mzfais@gmail.com>
2023-10-21 06:27:31 +0700

    tambah gesture handler di root view

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import CustomSafeArea from './src/components/CustomSafeArea';
 import Routes from './src/routes';
 import {PaperProvider} from 'react-native-paper';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 export default function App() {
   return (
     <CustomSafeArea>
       <PaperProvider>
-        <Routes />
+        <GestureHandlerRootView style={{flex: 1}}>
+          <Routes />
+        </GestureHandlerRootView>
       </PaperProvider>
     </CustomSafeArea>
   );
