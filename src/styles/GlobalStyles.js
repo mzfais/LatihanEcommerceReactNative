9fba46762fcc614f72fe9ecd3ff7b67d0c49f555
mzfais <mzfais@gmail.com>
2023-11-04 05:54:49 +0700

    update globalstyles

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -9,4 +9,24 @@ export const GlobalStyles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
   },
+  smallFont: {
+    fontSize: 14,
+    fontWeight: '400',
+  },
+  regularFont: {
+    fontSize: 16,
+    fontWeight: '400',
+  },
+  mediumFont: {
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  largeFont: {
+    fontSize: 22,
+    fontWeight: '700',
+  },
+  superLargeFont: {
+    fontSize: 28,
+    fontWeight: '700',
+  },
 });
